d78a1d63015b0b3d11b7cba7a2b33ed465b96112
unknown <giorgivarsimashvili814@gmail.com>
2026-03-26 01:49:50 +0400

    feat: trust proxy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,13 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy', 1);
 
   app.use(cookieParser());
 
